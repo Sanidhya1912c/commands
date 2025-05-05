73f86d366b19935260e705a008c1e089c5954c85
sanidhya1912c <sanidhyac1912@gmail.com>
2025-05-05 08:13:15 +0530

    .

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+
+
+# echo "# commands" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/Sanidhya1912c/commands.git
+# git push -u origin main
